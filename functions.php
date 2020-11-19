ad51b516db3bccca84eb3a1ede4a7fa5344749fe
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-11-18 23:08:53 -0600

    Added formatting to header.php file and style.scss from the live demo

--- functions.php
+++ functions.php
@@ -114,3 +114,11 @@ return count( $comments_by_type['comment'] );
 return $count;
 }
 }
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+  if (in_array('current-menu-item', $classes) ){
+    $classes[] = 'active ';
+  }
+  return $classes;
+}
