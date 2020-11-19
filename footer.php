b61953ed268dc6ac4e0e4d6e57fbd90afcf4453d
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-11-18 23:31:30 -0600

    Added formatting to footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,25 @@
-</div>
-<footer id="footer">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'simery' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-</div>
+</main>
+<footer>
+<div class="content-wrapper">
+    <div class="footer-content">
+      <aside>
+      <i class="fas fa-moon"></i>
+        &copy; <?php echo esc_html( date_i18n( __( 'Y', 'simery' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+        <h1>Simery</h1>
+        be the first to know when my mods go live!
+        <form>
+          <input type="email" placeholder="name@email.com"></input>
+          <input type="name" placeholder="your name"></input>
+          <input type="submit" placeholder="Submit"></input>
+        </form>
+      </aside>
+      <aside class="footer-social-media">
+        <a href="https://twitter.com/simerymods" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://github.com/emmy-html" target="_blank"><i class="fab fa-github-alt"></i></a>
+        <i class="fab fa-tumblr"></i>
+      </aside>
+    </div>
 </footer>
-</div>
 <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -7,13 +7,12 @@
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@200;400;900&family=Space+Mono:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body <?php body_class(); ?>>
-<div id="wrapper" class="hfeed">
 <header>
-<div class="content-wrapper">
-<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><i class="fas fa-moon"></i></a>
-<nav>
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-</nav>
-</div>
+    <div class="content-wrapper">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><i class="fas fa-moon"></i></a>
+        <nav>
+            <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+        </nav>
+    </div>  
 </header>
-<div id="container">
+<main>
